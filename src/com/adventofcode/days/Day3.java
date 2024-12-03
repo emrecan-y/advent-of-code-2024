83b212673a3bfa72a00e60614e7309b2d7c4ae0e
emrecan-y <emrecanyilmaz1997@gmail.com>
2024-12-03 14:33:37 +0100

    refactor the solution

--- src/com/adventofcode/days/Day3.java
+++ src/com/adventofcode/days/Day3.java
@@ -26,7 +26,7 @@ public class Day3 extends AbstractAdventDay {
     public void partTwo() {
         // the line breaks need to be disregarded when checking,
         // so the String List needs to be concatinated
-        long multiplicationResult = getCorruptedMultiplicationResultWithDoDOnt(
+        long multiplicationResult = getEnabledCorruptedMultiplicationResult(
                 super.getInput().stream().collect(Collectors.joining()));
         System.out.println("The enabled multiplication result is " + multiplicationResult);
     }
@@ -40,44 +40,52 @@ public class Day3 extends AbstractAdventDay {
             }
             s = s.substring(beginIndex);
             int endIndex = s.indexOf(")") + 1;
-            sum += getSingleMultiplicationResult(s.substring(0, endIndex));
-            s = s.substring(4);
+            sum += resolveMultiplicationStatement(s.substring(0, endIndex));
+            s = s.substring("mul(".length());
         }
         return sum;
     }
 
-    private long getSingleMultiplicationResult(String s) {
+    private long getEnabledCorruptedMultiplicationResult(String s) {
+        long sum = 0;
+        // "don't()" followed by a "do()" with inbetween;
+        // "." -> any character
+        // "*" -> 0 or more matches
+        // "?" -> lazy, so matches as few charactes as possible
+        // so only the valid parts of the string stay after splitting
+        for (String substring : s.split("don't\\(\\).*?do\\(\\)")) {
+            sum += getCorruptedMultiplicationResult(substring);
+        }
+        return sum;
+    }
+
+    private long resolveMultiplicationStatement(String s) {
         int maxValidLength = "mul(123,321)".length();
         int minValidLength = "mul(1,3)".length();
         if (s.length() > maxValidLength || s.length() < minValidLength) {
             return 0;
         }
-        s = s.substring(4, s.length() - 1);
-        if (!s.contains(",")) {
-            return 0;
-        } else {
+        s = s.substring("mul(".length(), s.length() - 1);
+        if (s.contains(",")) {
             String[] numbers = s.split(",");
-            try {
-                int num1 = Integer.parseInt(numbers[0]);
-                int num2 = Integer.parseInt(numbers[1]);
-                if (num1 <= 999 && num2 <= 999) {
-                    return num1 * num2;
-                }
-            } catch (NumberFormatException nfe) {
+            return multiplicateStrings(numbers[0], numbers[1]);
+        } else {
+            return 0;
+        }
+    }
+
+    private long multiplicateStrings(String s1, String s2) {
+        try {
+            int num1 = Integer.parseInt(s1);
+            int num2 = Integer.parseInt(s2);
+            if (num1 <= 999 && num2 <= 999) {
+                return num1 * num2;
+            } else {
                 return 0;
             }
+        } catch (NumberFormatException nfe) {
+            return 0;
         }
-        return 0;
     }
 
-    private long getCorruptedMultiplicationResultWithDoDOnt(String s) {
-        long sum = 0;
-        // split the string where there is "don't()" followed by a "do()" with any
-        // characters in between, so only the valid parts of the string stay
-        for (String ss : s.split("don't\\(\\).*?do\\(\\)")) {
-            sum += getCorruptedMultiplicationResult(ss);
-
-        }
-        return sum;
-    }
 }
