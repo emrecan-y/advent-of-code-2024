bd19693d1352dae8b1dcc5b7409bd544ad6e6a48
emrecan-y <emrecanyilmaz1997@gmail.com>
2024-12-04 20:56:50 +0100

    refactor the solution

--- src/com/adventofcode/days/Day4.java
+++ src/com/adventofcode/days/Day4.java
@@ -12,12 +12,12 @@ import com.adventofcode.AbstractAdventDay;
 public class Day4 extends AbstractAdventDay {
 
     public Day4() {
-        super(0);
+        super(4);
     }
 
     public static void main(String[] args) {
         Day4 day = new Day4();
-        // day.partOne();
+        day.partOne();
         day.partTwo();
     }
 
@@ -27,23 +27,27 @@ public class Day4 extends AbstractAdventDay {
                 .map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toList()))
                 .collect(Collectors.toList());
         int result = countXmasIn2dList(input2d);
-        System.out.println(result);
+        System.out.format("'XMAS' appears %d times\n", result);
+    }
 
-        // 2490 - 2531
+    @Override
+    public void partTwo() {
+        List<List<Character>> input2d = super.getInput().stream()
+                .map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        int result = getMasCount(input2d);
+        System.out.format("'X-MAS' appears %d times\n", result);
     }
 
     private int countXmasIn2dList(List<List<Character>> input2d) {
         int count = 0;
-        List<List<Character>> input2dTransposed = transposeList(input2d);
         // horizontal count
         count += input2d.stream().mapToInt(l -> countXmasInList(l)).sum();
         // vertical count
-        count += input2dTransposed.stream().mapToInt(l -> countXmasInList(l)).sum();
+        count += transposeList(input2d).stream().mapToInt(l -> countXmasInList(l)).sum();
         // diagonal count
-        count += getDiagonalList(input2d).stream().mapToInt(l -> countXmasInList(l)).sum();
-        printList(getDiagonalList(input2d));
-        flipList(input2d);
-        count += getDiagonalList(input2d).stream().mapToInt(l -> countXmasInList(l)).sum();
+        count += getDiagonalsList(input2d).stream().mapToInt(l -> countXmasInList(l)).sum();
+        count += getDiagonalsList(flipList(input2d)).stream().mapToInt(l -> countXmasInList(l)).sum();
         return count;
 
     }
@@ -51,36 +55,38 @@ public class Day4 extends AbstractAdventDay {
     private int countXmasInList(List<Character> list) {
         int count = 0;
         for (int i = 0; i < list.size() - 3; i++) {
-            if (list.get(i) == 'X' && list.get(i + 1) == 'M' && list.get(i + 2) == 'A' && list.get(i + 3) == 'S' ||
-                    list.get(i) == 'S' && list.get(i + 1) == 'A' && list.get(i + 2) == 'M' && list.get(i + 3) == 'X') {
-                count++;
+            StringBuilder toCheck = new StringBuilder();
+            for (int k = 0; k < 4; k++) {
+                toCheck.append(list.get(i + k));
             }
+            String toCheckString = toCheck.toString();
+            if (toCheckString.equals("XMAS") || toCheckString.equals("SAMX"))
+                count++;
         }
         return count;
     }
 
-    private <T> List<List<T>> getDiagonalList(List<List<T>> list2d) {
-        List<List<T>> diags = new ArrayList<>();
+    private <T> List<List<T>> getDiagonalsList(List<List<T>> list2d) {
+        List<List<T>> diagonalsList = new ArrayList<>();
         int rows = list2d.size();
         int cols = list2d.get(0).size();
         // Get diagonals starting from the first row
         for (int col = 0; col < cols; col++) {
-            List<T> diag = new ArrayList<>();
+            List<T> diagonal = new ArrayList<>();
             for (int i = 0, j = col; i < rows && j < cols; i++, j++) {
-                diag.add(list2d.get(i).get(j));
+                diagonal.add(list2d.get(i).get(j));
             }
-            diags.add(diag);
+            diagonalsList.add(diagonal);
         }
         // Get diagonals starting from the first column - top row
         for (int row = 1; row < rows; row++) {
-            List<T> diag = new ArrayList<>();
+            List<T> diagonals = new ArrayList<>();
             for (int i = row, j = 0; i < rows && j < cols; i++, j++) {
-                diag.add(list2d.get(i).get(j));
+                diagonals.add(list2d.get(i).get(j));
             }
-            diags.add(diag);
+            diagonalsList.add(diagonals);
         }
-
-        return diags;
+        return diagonalsList;
     }
 
     private <T> List<List<T>> transposeList(List<List<T>> list2d) {
@@ -95,37 +101,19 @@ public class Day4 extends AbstractAdventDay {
         return transposed;
     }
 
-    private <T> void flipList(List<List<T>> list2d) {
-        list2d.stream().forEach(list -> Collections.reverse(list));
-    }
-
-    private <T> void printList(List<List<T>> list2d) {
-        for (List<T> list : list2d) {
-            for (T listItem : list) {
-                if (listItem != null) {
-                    System.out.print(listItem);
-                } else {
-                    System.out.print(" ");
-
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    @Override
-    public void partTwo() {
-        List<List<Character>> input2d = super.getInput().stream()
-                .map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toList()))
-                .collect(Collectors.toList());
-        int result = getMasCount(input2d);
-        System.out.println(result);
+    private <T> List<List<T>> flipList(List<List<T>> list2d) {
+        List<List<T>> flipped = new ArrayList<>(list2d);
+        flipped.stream().forEach(list -> Collections.reverse(list));
+        return flipped;
     }
 
     private int getMasCount(List<List<Character>> input2d) {
         int count = 0;
         for (int y = 1; y < input2d.size() - 1; y++) {
             for (int x = 1; x < input2d.get(y).size() - 1; x++) {
+                // |1 2| |M S| |M M| |S M| |S S|
+                // | 3 | | A | | A | | A | | A |
+                // |4 5| |M S| |S S| |S M| |M M|
                 List<Character> mas = new ArrayList<>();
                 mas.add(input2d.get(y - 1).get(x - 1));
                 mas.add(input2d.get(y - 1).get(x + 1));
@@ -141,7 +129,6 @@ public class Day4 extends AbstractAdventDay {
     }
 
     private boolean isValidMas(List<Character> mas) {
-        // topLeft, topRight, middle, bottomLeft, bottomRight
         Set<String> validMases = new HashSet<>();
         validMases.add("MSAMS");
         validMases.add("MMASS");
