71140544d4945e23c834bf69ff9f0775159e7aa9
emrecan-y <emrecanyilmaz1997@gmail.com>
2024-12-05 19:44:23 +0100

    refactor the solution

--- src/com/adventofcode/days/Day5.java
+++ src/com/adventofcode/days/Day5.java
@@ -1,15 +1,12 @@
 package com.adventofcode.days;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import com.adventofcode.AbstractAdventDay;
@@ -33,12 +30,11 @@ public class Day5 extends AbstractAdventDay {
         List<List<String>> pageNumbersList = new ArrayList<>();
         parseInput(orderingMap, pageNumbersList);
 
-        int sum = 0;
-        for (List<String> pageNumbers : pageNumbersList) {
-            if (isValidPage(pageNumbers, orderingMap)) {
-                sum += Integer.parseInt(pageNumbers.get(pageNumbers.size() / 2));
-            }
-        }
+        int sum = pageNumbersList.stream()
+                .filter(p -> isValidPageList(p, orderingMap))
+                .mapToInt(p -> Integer.parseInt(p.get(p.size() / 2)))
+                .sum();
+
         System.out.println("The sum of middle page numbers from the correctly-ordered lists is " + sum);
     }
 
@@ -48,30 +44,13 @@ public class Day5 extends AbstractAdventDay {
         List<List<String>> pageNumbersList = new ArrayList<>();
         parseInput(orderingMap, pageNumbersList);
 
-        int sum = 0;
-        for (List<String> pageNumbers : pageNumbersList) {
-            if (!isValidPage(pageNumbers, orderingMap)) {
-                List<String> orderedNumbers = orderPage(pageNumbers, orderingMap);
-                sum += Integer.parseInt(orderedNumbers.get(orderedNumbers.size() / 2));
-            }
-        }
-        System.out.println("The sum of middle page numbers from the not correctly-ordered lists is " + sum);
-    }
+        int sum = pageNumbersList.stream()
+                .filter(p -> !isValidPageList(p, orderingMap))
+                .map(p -> getCorrectlyOrderedList(p, orderingMap))
+                .mapToInt(p -> Integer.parseInt(p.get(p.size() / 2)))
+                .sum();
 
-    private boolean isValidPage(List<String> pageNumbers, Map<String, Set<String>> orderingMap) {
-        // check from back
-        for (int i = pageNumbers.size() - 1; i >= 0; i--) {
-            Set<String> set = orderingMap.get(pageNumbers.get(i));
-            if (set == null) {
-                continue;
-            }
-            for (int k = i; k >= 0; k--) {
-                if (set.contains(pageNumbers.get(k))) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        System.out.println("The sum of middle page numbers from the not correctly-ordered lists is " + sum);
     }
 
     private void parseInput(Map<String, Set<String>> orderingMap, List<List<String>> pageNumbersList) {
@@ -95,9 +74,25 @@ public class Day5 extends AbstractAdventDay {
         }
     }
 
-    private List<String> orderPage(List<String> pageNumbers, Map<String, Set<String>> orderingMap) {
-        // same algorithm from isValidPage(), it just swaps the two current indices in
-        // the list when there is a wrong ordered pair
+    private boolean isValidPageList(List<String> pageNumbers, Map<String, Set<String>> orderingMap) {
+        // check from back
+        for (int i = pageNumbers.size() - 1; i >= 0; i--) {
+            Set<String> set = orderingMap.get(pageNumbers.get(i));
+            if (set == null) {
+                continue;
+            }
+            for (int k = i; k >= 0; k--) {
+                if (set.contains(pageNumbers.get(k))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<String> getCorrectlyOrderedList(List<String> pageNumbers, Map<String, Set<String>> orderingMap) {
+        // same algorithm from isValidPageList(), it just swaps the two current
+        // indices in the list when there is a wrong ordered pair
         List<String> ordered = new ArrayList<>(pageNumbers);
         for (int i = ordered.size() - 1; i >= 0; i--) {
             Set<String> set = orderingMap.get(ordered.get(i));
